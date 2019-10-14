efe992704e31bae9313edfef83627ad6cab44ffb
Sandy Toh <sandytoh@u.nus.edu>
2019-10-14 10:31:14 +0800

    Validation

--- src/app/components/form.component.ts
+++ src/app/components/form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import * as moment from 'moment';
 
 function ageRangeValidator(control: AbstractControl): { [key: string]: boolean } | null {
   if (control.value !== undefined && (isNaN(control.value) || control.value < 18 || control.value > 45)) {
@@ -17,6 +17,14 @@ function ageRangeValidator2(min: number, max: number): ValidatorFn {
           return { ageRange: true };
       }
       return null;
+  };
+}
+
+function ageValidator(min: number): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const age = moment().diff(control.value, 'years');
+    console.log('age', age);
+    return age < min ? {ageValidation: {value: control.value}} : null;
   };
 }
 
@@ -42,19 +50,25 @@ export class FormComponent implements OnInit {
   get f() { return this.contactForm.controls; }
 
   createFormGroup() {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#\$])(?=.{8,})/;
+    const contactPattern = /^[0-9\+\-\)\(]*$/;
+
     return new FormGroup({
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8),
+      Validators.pattern(passwordPattern)]),
+      gender: new FormControl('', [Validators.required]),
+      dob: new FormControl('', [Validators.required, ageValidator(18)]),
+      address: new FormControl('', [Validators.required]),
       country: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      custom: new FormControl('', [Validators.required, ageRangeValidator2(5, 10)])
+      contact: new FormControl('', [Validators.required, Validators.pattern(contactPattern)])
     });
   }
 
-  cancel() {
+  reset() {
     this.contactForm.reset();
-    this.router.navigate(['/list']);
+    this.router.navigate(['/add'], { fragment: 'main' });
   }
 
   onSubmit() {
---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id?: number;
+    name: string;
+    email: string;
+    password: string;
+    gender: string;
+    dob: string;
+    address: string;
+    contact: string;
+    country: string;
+}
